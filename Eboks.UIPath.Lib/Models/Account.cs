b13fd8bb0d3884f7f7323d2aaa4704b7fb7a4dcf
Frederik Lundbeck Jørgensen <frl@e-Boks.dk>
2019-09-24 14:35:13 +0200

    Modified Account.cs, Company.cs

---
+++ Eboks.UIPath.Lib/Models/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eboks.UIPath.Lib.Models
+{
+    class Account
+    {
+        public string Number { get; set; }
+        public string Name { get; set; }
+        public string AccountType { get; set; }
+        public string EntryType { get; set; }
+        public string CompanyEntryGroup { get; set; }
+        public string ProductEntryGroup { get; set; }
+        public double NetChange { get; set; }
+        public double Balance { get; set; }
+        public List<Entry> Entries { get; }
+
+        public Account(){}
+
+        public override string ToString()
+        {
+            return $"{Name}, {Number}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Account c = (Account)obj;
+            return Name.Equals(c.Name) && Number.Equals(c.Number);
+        }
+
+    }
+}
---
+++ Eboks.UIPath.Lib/Models/Entry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eboks.UIPath.Lib.Models
+{
+    class Entry
+    {
+        public string EntryDate { get; set; }
+        public string AttachmentType { get; set; }
+        public string AttachmentNumber { get; set; }
+        public string GeneralLedgerNumber { get; set; }
+        public string Description { get; set; }
+        public string CompanyEntryGroup { get; set; }
+        public string ProductEntryGroup { get; set; }
+        public double Amount { get; set; }
+        public string DepartmentCode { get; set; }
+        public string CountryCode { get; set; }
+        public string ActivitiesCode { get; set; }
+        public double VatAmount { get; set; }
+
+        public Entry()
+        {
+
+        }
+    }
+}
