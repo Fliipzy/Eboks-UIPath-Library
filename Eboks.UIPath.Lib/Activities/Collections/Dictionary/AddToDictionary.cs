db4a34172c97e5bef25055aa4b16a718f028f12e
Frederik L. Jørgensen <Frederiklundbeck@live.dk>
2019-09-28 00:10:17 +0200

    Modified AddToDictionary activity class, updated nuspec for 1.0.1.1, added liblogo.png to /logo

--- Eboks.UIPath.Lib/Activities/Collections/Dictionary/AddToDictionary.cs
+++ Eboks.UIPath.Lib/Activities/Collections/Dictionary/AddToDictionary.cs
@@ -8,18 +8,20 @@ using System.ComponentModel;
 
 namespace Eboks.UIPath.Lib.Activities.Collections.Dictionary
 {
+    [DisplayName("Add to dictionary"), DesignerCategory("Collections")]
+    [Description("Adds a KeyValuePair to a dictionary.")]
     public class AddToDictionary<T,U> : CodeActivity
     {
-        [Category("Input")]
-        [RequiredArgument]
+        [Category("Input"), DisplayName("Dictionary Target"), RequiredArgument]
+        [Description("The target dictionary, which the KeyValuePair will be added to.")]
         public InArgument<Dictionary<T, U>> Dictionary { get; set; }
 
-        [Category("Input")]
-        [RequiredArgument]
+        [Category("Input"), RequiredArgument]
+        [Description("The key object, to be added.")]
         public InArgument<T> Key { get; set; }
 
-        [Category("Input")]
-        [RequiredArgument]
+        [Category("Input"), RequiredArgument]
+        [Description("The value object, to be added.")]
         public InArgument<U> Value { get; set; }
 
         protected override void Execute(CodeActivityContext context)
